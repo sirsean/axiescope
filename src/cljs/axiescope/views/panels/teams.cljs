96d85841a57cf40fcdce483804ec648db8e2be91
Sean Schulte <sirsean@gmail.com>
2019-08-04 07:38:03 -0500

    new teams page

--- src/cljs/axiescope/views/panels/teams.cljs
+++ src/cljs/axiescope/views/panels/teams.cljs
@@ -14,33 +14,56 @@
     [:div.container
      [header {:title "Teams"
               :bars [:teams]}]
-     (if loading?
+     (if (and loading?
+              (<= (count teams) 20))
        [:div.row
         [:div.col-xs-12.center-xs
          [:em "loading teams..."]]]
        [:div.row
-        [:div.col-xs-12
-         (doall
-           (for [t teams]
-             [:div.row {:key (:team-id t)}
+        (doall
+          (for [t teams]
+            [:div.col-xs-4 {:key (:team-id t)
+                            :style {:padding "20px"}}
+             [:div.row
               [:div.col-xs-12
-               [:div.row {:style {:background-color "#DDDDDD"
-                                  :padding "0.4em 0"}}
-                [:div.col-xs-4
-                 [:a {:href (format "https://axieinfinity.com/team/%s" (:team-id t))
-                      :target "_blank"}
-                  [:strong (:name t)]]]
-                [:div.col-xs-4.center-xs]
-                [:div.col-xs-4.end-xs
-                 (if (:ready? t)
-                   [:em "ready"]
-                   [:span (format "ready in %sm" (:ready-in t))])]]
-               [:div.row
-                [:div.col-xs-12
-                 (let [axies @(rf/subscribe [:teams/team-axies (:team-id t)])]
-                   [rdt/data-table
-                    {:headers axie-table-headers
-                     :rows axies
-                     :td-render-fn axie-row-render-fn}])]]]]))]
-        ])
+               [:a {:href (format "https://axieinfinity.com/team/%s" (:team-id t))
+                    :target "_blank"}
+                [:strong (:name t)]]]]
+             [:div.row {:style {:font-size "0.8em"}}
+              [:div.col-xs-6]
+              [:div.col-xs-2.end-xs "Tank"]
+              [:div.col-xs-2.end-xs "DPS"]
+              [:div.col-xs-2.end-xs "Support"]]
+             (for [{:keys [id name image
+                           tank-body dps-body support-body]}
+                   @(rf/subscribe [:teams/team-axies (:team-id t)])
+                   :when (some? id)]
+               [:div.row.middle-xs {:key id}
+                [:div.col-xs-3
+                 [:img {:src image
+                        :style {:width "100%"}}]]
+                [:div.col-xs-3
+                 [:a {:href (format "/axie/%s" id)}
+                  name]]
+                [:div.col-xs-2.end-xs
+                 {:style (if (<= 4 tank-body)
+                           {:color "green"
+                            :font-weight "bold"}
+                           {:color "#444444"
+                            :font-size "0.9em"})}
+                 tank-body]
+                [:div.col-xs-2.end-xs
+                 {:style (if (<= 4 dps-body)
+                           {:color "green"
+                            :font-weight "bold"}
+                           {:color "#444444"
+                            :font-size "0.9em"})}
+                 dps-body]
+                [:div.col-xs-2.end-xs
+                 {:style (if (<= 3 support-body)
+                           {:color "green"
+                            :font-weight "bold"}
+                           {:color "#444444"
+                            :font-size "0.9em"})}
+                 support-body]])]))])
      [footer]]))
